4c08a712e9287631d2de830de14a51ce19227a46
Teka101 <pi@raspberrypi.(none)>
2014-07-20 19:12:46 +0200

    Remove sentences.

--- classes/SpeechRecognize.fr.class.php
+++ classes/SpeechRecognize.fr.class.php
@@ -6,10 +6,4 @@
 	$SR_WORDS['DURATION_WEEK'] = array('semaine', 'semaines');
 	$SR_WORDS['DURATION_MONTH'] = array('mois');
 	$SR_WORDS['NUMBERS'] = json_decode('{ "1": ["un", "une"], "2": ["deux"], "3": ["trois"], "4": ["quatre"], "5": ["cinq"], "6": ["six"], "7": ["sept"], "8": ["huit"], "9": ["neuf"] }');
-
-	$SR_SENTENCES = array();
-	$SR_SENTENCES[] = array('text' => '^EAGGER_1$', 'action' => 'cbEagger');
-	$SR_SENTENCES[] = array('text' => '^TO_LEAVE ([0-9]+) DURATION_DAY$', 'action' => 'cbLeaveHouseDay');
-	$SR_SENTENCES[] = array('text' => '^TO_LEAVE ([0-9]+) DURATION_WEEK$', 'action' => 'cbLeaveHouseWeek');
-	$SR_SENTENCES[] = array('text' => '^TO_LEAVE ([0-9]+) DURATION_MONTH$', 'action' => 'cbLeaveHouseMonth');
 ?>
