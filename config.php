71dbdc4aa02a297a373ec6eed27b3ca9a05119ae
Teka101 <pi@raspberrypi.(none)>
2014-07-20 19:55:17 +0200

    Update config

--- config.php
+++ config.php
@@ -3,7 +3,10 @@
 setlocale(LC_ALL, 'fr_FR.utf8');
 ini_set('display_errors','1');
 
+define('SPEAK_LANGUAGE', 'fr');
 define('DOMOTICZ_SERVER', 'http://127.0.0.1:8080');
 define('DOMOTICZ_HEATING_IDX', '16');
+
+define('PHILIPS_TV', '192.168.0.3');
 
 ?>
