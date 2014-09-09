0ee5b8fe808d9bfb0a6b5213220edbeaf33959d6
Teka101 <pi@raspberrypi.(none)>
2014-09-09 12:26:48 +0200

    Now update will get temperature from DHT22 and put it in Domoticz.

--- classes/Domoticz.class.php
+++ classes/Domoticz.class.php
@@ -2,6 +2,16 @@
 
 class Domoticz
 {
+	static public function pushHomeHumTemp($hum, $temp)
+	{
+		$ch = curl_init(DOMOTICZ_SERVER . '/json.htm?type=command&param=udevice&idx=' . DOMOTICZ_HEAT_HOME_IDX . '&nvalue=0&svalue=' . $temp . ';' . $hum . ';2');
+		if ($ch)
+		{
+			curl_exec($ch);
+			curl_close($ch);
+		}
+	}
+
 	static public function pushTemperature($temp)
 	{
 		$ch = curl_init(DOMOTICZ_SERVER . '/json.htm?type=command&param=udevice&idx=' . DOMOTICZ_HEATING_IDX . '&nvalue=0&svalue=' . $temp);
--- update.php
+++ update.php
@@ -4,6 +4,35 @@ require 'config.php';
 require 'classes/Domoticz.class.php';
 require 'classes/Heating.class.php';
 
+$oldTemp = null;
+$newTemp = null;
+$oldHumidity = null;
+$newHumidity = null;
+
+if (($hdl = popen('/bin/cat /home/pi/DHT22.txt', 'r')))
+{
+	while (($str = fgets($hdl)))
+		if (preg_match('/^Humidity = ([0-9\.]+) % Temperature = ([0-9\.]+) \*C/', $str, $m) == 1)
+		{
+			$newHumidity = $m[1];
+			$newTemp = $m[2];
+		}
+	pclose($hdl);
+}
+
+$diffHum = ($oldHumidity == null ? 0 : abs($oldHumidity - $newHumidity));
+$diffTemp = ($oldTemp == null ? 0 : abs($oldTemp - $newTemp));
+
+print "Home temperature: $newTemp [old=$oldTemp diff=$diffTemp]<br>\n";
+print "Home humidity: $newHumidity [old=$oldHumidity diff=$diffHum]<br>\n";
+if (($diffHum <= 10 && $diffHum <= 5)
+	&& ($newTemp != null && $newHumidity != null))
+{
+	echo "Sending home informations...<br>\n";
+	Domoticz::pushHomeHumTemp($newHumidity, $newTemp);
+	echo "<br>\n";
+}
+
 $heating = new Heating();
 $temperature = $heating->getCurrentTemperature();
 
