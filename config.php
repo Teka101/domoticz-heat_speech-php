8afda86508c5d1a497baa6048bb3cc26a5e5c6b5
Teka101 <pi@raspberrypi.(none)>
2014-09-09 12:27:46 +0200

    Update home config.

--- config.php
+++ config.php
@@ -5,8 +5,8 @@ ini_set('display_errors','1');
 
 define('SPEAK_LANGUAGE', 'fr');
 define('DOMOTICZ_SERVER', 'http://127.0.0.1:8080');
-define('DOMOTICZ_HEAT_HOME_IDX', '15');
-define('DOMOTICZ_HEATING_IDX', '16');
+define('DOMOTICZ_HEAT_HOME_IDX', '8');
+define('DOMOTICZ_HEATING_IDX', '9');
 
 define('PHILIPS_TV', '192.168.0.3');
 
